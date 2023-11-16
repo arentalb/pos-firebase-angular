70a5525873252767dab0f4be6a7866282cfa5137
aren talb <aren.talb00@gmail.com>
2023-11-16 21:11:23 +0300

    Add swUpdate to check for new version of service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router} from "@angular/router";
+import {SwUpdate} from "@angular/service-worker";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pos';
+  constructor(private router: Router ,private swUpdate: SwUpdate) {
+
+
+  }
+
+
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+        if(confirm("New version available. Load New Version?")) {
+
+          window.location.reload();
+        }
+      });
+    }
+
+  }
+
 }
+
