2b4634471d3772873c29f47b2fc4d6da24f08eb1
aren talb <aren.talb00@gmail.com>
2023-11-25 17:12:21 +0300

    Add bat file

---
+++ run-app.bat
@@ -0,0 +1,14 @@
+@echo off
+cd C:\Users\Click\Documents\angular\code\pos
+cd dist
+cd ..
+call ng build --configuration=production
+cd dist/pos
+
+:: Stop the HTTP server if it's already running
+taskkill /F /IM http-server >nul 2>&1
+
+http-server -c-1 --proxy http://localhost:8083/firebase-api -p 9001
+
+
+
